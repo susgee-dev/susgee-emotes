4cb03061938222dde3e996b37ac576cbf712a3b6
maersux <marcel@gertsch.be>
2025-07-08 23:38:09 +0200

    chore: proper susgee-link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import { Link } from '@/components/ui/link';
 export default function Footer() {
 	return (
 		<footer>
-			<p className="text-center">
+			<p className="px-1 text-center">
 				Ideas for improvements? Discovered a bug? Feel free to create an{' '}
 				<Link href="https://github.com/susgee-dev/susgee-emotes/issues/new" target="_blank">
 					issue on our GitHub
@@ -14,7 +14,10 @@ export default function Footer() {
 			</p>
 			<div className="flex flex-col justify-center gap-1 p-2 text-center text-font-dark md:flex-row md:gap-2">
 				<p>
-					&copy; {new Date().getFullYear()} <Link href="/">susgee.dev</Link>
+					&copy; {new Date().getFullYear()}{' '}
+					<Link href="https://susgee.dev" target="_blank">
+						susgee.dev
+					</Link>
 				</p>
 				<span className="hidden md:flex"> • </span>
 				<p>not affiliated with twitch</p>
